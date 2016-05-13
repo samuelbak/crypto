5e5810326c1780932dcb46820ce5f77f9fa61630
Samuele Bacchetta <samuele1988@gmail.com>
2016-05-13 13:43:43 +0200

    ok

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -1,9 +1,22 @@
 package crypto;
 
+import java.io.File;
+
 public class Crypto {
 
 	public static void main(String[] args) {
-		System.out.println("Ciao");
+		String key = "Mary has one cat";
+        File inputFile = new File("document.txt");
+        File encryptedFile = new File("document.encrypted");
+        File decryptedFile = new File("document.decrypted");
+         
+        try {
+            CryptoUtils.encrypt(key, inputFile, encryptedFile);
+            CryptoUtils.decrypt(key, encryptedFile, decryptedFile);
+        } catch (CryptoException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
 
 	}
 
